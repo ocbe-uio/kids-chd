2ce1486f02f563d966a47438853b466bc684382e
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-25 14:13:30 +0200

    Renamed backend variables

--- src/server.R
+++ src/server.R
@@ -7,12 +7,12 @@ server <- function(input, output) {
     NA
   }
 
-  heart_rate <- function(diagnosis, height, bmi, sex) {
-    software <- 0.0469 # prop. of Vyntus on Fontan (only relevant case here)
+  heart_rate <- function(diagnosis, height, bmi, is_male) {
+    is_vyntus <- 0.0469 # prop. of Vyntus on Fontan (only relevant case here)
     switch(diagnosis,
-      simple = (9168804 * height + 5.13e9) ^ (1 / 4.3),
-      moderate = (9.9e8 * height - 2.86e9 * bmi + 1.4e11) ^ (1 / 5),
-      fontan = (-144400.5 * height - 3.81e7 * sex + 2076971 * bmi * sex + 1.24e7 * software + 9.75e7) ^ (1 / 3.5)
+      "simple" = (9168804 * height + 5.13e9) ^ (1 / 4.3),
+      "moderate" = (9.9e8 * height - 2.86e9 * bmi + 1.4e11) ^ (1 / 5),
+      "fontan" = (-144400.5 * height - 3.81e7 * is_male + 2076971 * bmi * is_male + 1.24e7 * is_vyntus + 9.75e7) ^ (1 / 3.5)
     )
   }
 
