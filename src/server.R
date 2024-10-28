4ea47a4d940d08fb9383b16e5ce1a664dc836e91
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-10-28 14:45:40 +0100

    Renamed variables for clarity (#3)

--- src/server.R
+++ src/server.R
@@ -1,9 +1,9 @@
 diagnosis <- setRefClass(
   "Diagnosis",
   fields = list(
-    hosp_soft = "numeric", # cross-proportion of surgical and vyntus
-    hosp = "numeric",  # proportion of surgical centres
-    soft = "numeric",  # proportion of vyntus software
+    surg_vyntus = "numeric", # cross-proportion of surgical and vyntus
+    surg = "numeric",  # proportion of surgical centres
+    vyntus = "numeric",  # proportion of vyntus software
     heart_rate = "function",
     breathing_frequency = "function"
   )
@@ -19,38 +19,38 @@ person <- setRefClass(
 )
 
 simple <- diagnosis(
-  hosp_soft = c(0.3994, 0.0029, 0.5478, 0.0499),
-  hosp = 0.5849,
-  soft = 0.0528,
+  surg_vyntus = c(0.3994, 0.0029, 0.5478, 0.0499),
+  surg = 0.5849,
+  vyntus = 0.0528,
   heart_rate = function(.self, person) {
     (9168804 * person$height + 5.13e9) ^ (1 / 4.3)
   },
   breathing_frequency = function(.self, person) {
-   (- 0.0114363 * person$height + 0.0007431 * person$sex - 0.1421088 * .self$hosp + 6.693345) ^ (1 / 0.4)
+   (- 0.0114363 * person$height + 0.0007431 * person$sex - 0.1421088 * .self$surg + 6.693345) ^ (1 / 0.4)
   }
 )
 
 moderate <- diagnosis(
-  hosp_soft = c(0.6439, 0.0060, 0.2938, 0.0563),
-  hosp = 0.6499,
-  soft = 0.0624,
+  surg_vyntus = c(0.6439, 0.0060, 0.2938, 0.0563),
+  surg = 0.6499,
+  vyntus = 0.0624,
   heart_rate = function(.self, person) {
     (9.9e8 * person$height - 2.86e9 * person$bmi + 1.4e11) ^ (1 / 5)
   },
   breathing_frequency = function(.self, person) {
-    (-0.037375 * person$height - 1.778892 * person$sex + 0.0134113 * person$height * person$sex - 0.3806323 * .self$hosp + 16.65239) ^ (1 / 0.6)
+    (-0.037375 * person$height - 1.778892 * person$sex + 0.0134113 * person$height * person$sex - 0.3806323 * .self$surg + 16.65239) ^ (1 / 0.6)
   }
 )
 
 fontan <- diagnosis(
-  hosp_soft = c(0.7834, 0.0072, 0.1697, 0.0397),
-  hosp = 0.7906,
-  soft = 0.0469,
+  surg_vyntus = c(0.7834, 0.0072, 0.1697, 0.0397),
+  surg = 0.7906,
+  vyntus = 0.0469,
   heart_rate = function(.self, person) {
-    (-144400.5 * person$height - 3.81e7 * person$sex + 2076971 * person$bmi * person$sex + 1.24e7 * .self$soft + 9.75e7) ^ (1 / 3.5)
+    (-144400.5 * person$height - 3.81e7 * person$sex + 2076971 * person$bmi * person$sex + 1.24e7 * .self$vyntus + 9.75e7) ^ (1 / 3.5)
   },
   breathing_frequency = function(.self, person) {
-    exp(-0.0044619 * person$height + 0.0225936 * log(person$bmi) * person$sex - 0.0820773 * .self$hosp + 4.609728)
+    exp(-0.0044619 * person$height + 0.0225936 * log(person$bmi) * person$sex - 0.0820773 * .self$surg + 4.609728)
   }
 )
 
