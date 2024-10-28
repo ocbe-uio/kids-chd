d839efe25244086c1760182a33e44e0ab266bf02
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-10-28 15:30:02 +0100

    Implemented both VO2 calculations (#3)

--- src/server.R
+++ src/server.R
@@ -4,6 +4,9 @@ diagnosis <- setRefClass(
     surg_vyntus = "numeric", # cross-proportion of surgical and vyntus
     surg = "numeric",  # proportion of surgical centres
     vyntus = "numeric",  # proportion of vyntus software
+    grid = "data.frame",
+    vo2_ml_min = "function",
+    vo2_ml_kg_min = "function",
     heart_rate = "function",
     breathing_frequency = "function"
   )
@@ -22,6 +25,19 @@ simple <- diagnosis(
   surg_vyntus = c(0.3994, 0.0029, 0.5478, 0.0499),
   surg = 0.5849,
   vyntus = 0.0528,
+  grid = expand.grid("vyntus" = 0:1, "surg" = 1:0),
+  vo2_ml_min = function(.self, person) {
+    results = apply(.self$grid, 1, function(config) {
+      exp(0.0155584 * person$height + 0.4371531 * log(person$bmi) + 0.0009139 * person$height * person$sex - 0.1803019 * config["vyntus"] + 0.102317 * config["surg"] + 3.760053)
+    })
+    weighted.mean(results, .self$surg_vyntus)
+  },
+  vo2_ml_kg_min = function(.self, person) {
+    results = apply(.self$grid, 1, function(config) {
+      0.0983195 * person$height - 1.152879 * person$bmi + 0.0423992 * person$height * person$sex - 7.601633 * config["vyntus"] + 4.73933 * config["surg"] + 45.77055
+    })
+    weighted.mean(results, .self$surg_vyntus)
+  },
   heart_rate = function(.self, person) {
     (9168804 * person$height + 5.13e9) ^ (1 / 4.3)
   },
@@ -34,6 +50,19 @@ moderate <- diagnosis(
   surg_vyntus = c(0.6439, 0.0060, 0.2938, 0.0563),
   surg = 0.6499,
   vyntus = 0.0624,
+  grid = expand.grid("vyntus" = 0:1, "surg" = 1:0),
+  vo2_ml_min = function(.self, person) {
+    results = apply(.self$grid, 1, function(config) {
+      exp(0.0136715 * person$height + 0.3949761 * log(person$bmi) + 0.0010347 * person$height * person$sex - 0.0829085 * config["vyntus"] + 0.088169 * config["surg"] + 4.069768)
+    })
+    weighted.mean(results, .self$surg_vyntus)
+  },
+  vo2_ml_kg_min = function(.self, person) {
+    results = apply(.self$grid, 1, function(config) {
+      -21.99611 * log(person$bmi) + 0.0430605 * person$height * person$sex - 3.504218 * config["vyntus"] + 3.483406 * config["surg"] + 99.9302
+    })
+    weighted.mean(results, .self$surg_vyntus)
+  },
   heart_rate = function(.self, person) {
     (9.9e8 * person$height - 2.86e9 * person$bmi + 1.4e11) ^ (1 / 5)
   },
@@ -46,6 +75,19 @@ fontan <- diagnosis(
   surg_vyntus = c(0.7834, 0.0072, 0.1697, 0.0397),
   surg = 0.7906,
   vyntus = 0.0469,
+  grid = expand.grid("vyntus" = 0:1, "surg" = 1:0),
+  vo2_ml_min = function(.self, person) {
+    results = apply(.self$grid, 1, function(config) {
+      exp(0.0142453 * person$height + 0.3543394 * log(person$bmi) - 0.8410274 * person$sex + 0.3348171 * log(person$bmi) * person$sex - 0.1212021 * config["vyntus"] + 0.0948334 * config["surg"] + 3.929859)
+    })
+    weighted.mean(results, .self$surg_vyntus)
+  },
+  vo2_ml_kg_min = function(.self, person) {
+    results = apply(.self$grid, 1, function(config) {
+      -0.6682767 * person$bmi + 0.0329825 * person$height * person$sex - 4.52337 * config["vyntus"] + 3.745683 * config["surg"] + 42.38803
+    })
+    weighted.mean(results, .self$surg_vyntus)
+  },
   heart_rate = function(.self, person) {
     (-144400.5 * person$height - 3.81e7 * person$sex + 2076971 * person$bmi * person$sex + 1.24e7 * .self$vyntus + 9.75e7) ^ (1 / 3.5)
   },
@@ -55,13 +97,6 @@ fontan <- diagnosis(
 )
 
 server <- function(input, output) {
-  vo2_ml_min <- function() {
-    NA
-  }
-
-  vo2_ml_kg_min <- function() {
-    NA
-  }
   ventilation <- function() {
     NA
   }
@@ -85,8 +120,8 @@ server <- function(input, output) {
         "Oxygen pulse", "VE/VCO2 slope", "Breathing frequency"
       ),
       "Value" = c(
-        vo2_ml_min(),
-        vo2_ml_kg_min(),
+        diag$vo2_ml_min(diag, person),
+        diag$vo2_ml_kg_min(diag, person),
         diag$heart_rate(diag, person),
         ventilation(),
         oxygen_pulse(),
