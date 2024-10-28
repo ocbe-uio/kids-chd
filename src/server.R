ff4ea0c72d764cec97e9a50582fd2ce7b732f42d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-10-28 14:00:17 +0100

    Using OOP for heart_rate

--- src/server.R
+++ src/server.R
@@ -1,3 +1,44 @@
+diagnosis <- setRefClass(
+  "Diagnosis",
+  fields = list(
+    hosp_soft = "numeric", # cross-proportion of non-surgical and vyntus
+    hosp = "numeric",  # proportion of non-surgical centres
+    soft = "numeric",  # proportion of vyntus software
+    heart_rate = "function"
+  )
+)
+
+person <- setRefClass(
+  "Person",
+  fields = list(
+    sex = "numeric",
+    height = "numeric",
+    bmi = "numeric"
+  )
+)
+
+simple <- diagnosis(
+  hosp_soft = c(0.3994, 0.0029, 0.5478, 0.0499),
+  hosp = 0.5849,
+  soft = 0.0528,
+  heart_rate = function(.self, person) {
+    (9168804 * person$height + 5.13e9) ^ (1 / 4.3)
+  }
+)
+
+moderate <- diagnosis(
+  heart_rate = function(.self, person) {
+    (9.9e8 * person$height - 2.86e9 * person$bmi + 1.4e11) ^ (1 / 5)
+  }
+)
+
+fontan <- diagnosis(
+  soft = 0.0469,
+  heart_rate = function(.self, person) {
+    (-144400.5 * person$height - 3.81e7 * person$sex + 2076971 * person$bmi * person$sex + 1.24e7 * .self$soft + 9.75e7) ^ (1 / 3.5)
+  }
+)
+
 server <- function(input, output) {
   vo2_ml_min <- function() {
     NA
@@ -6,16 +47,6 @@ server <- function(input, output) {
   vo2_ml_kg_min <- function() {
     NA
   }
-
-  heart_rate <- function(diagnosis, height, bmi, is_male) {
-    is_vyntus <- 0.0469 # prop. of Vyntus on Fontan (only relevant case here)
-    switch(diagnosis,
-      "simple" = (9168804 * height + 5.13e9) ^ (1 / 4.3),
-      "moderate" = (9.9e8 * height - 2.86e9 * bmi + 1.4e11) ^ (1 / 5),
-      "fontan" = (-144400.5 * height - 3.81e7 * is_male + 2076971 * bmi * is_male + 1.24e7 * is_vyntus + 9.75e7) ^ (1 / 3.5)
-    )
-  }
-
   ventilation <- function() {
     NA
   }
@@ -33,6 +64,10 @@ server <- function(input, output) {
   }
 
   output$results_table <- renderTable({
+    diag <- get(input$diagnosis)
+    person <- person(
+      sex = as.numeric(input$sex), height = input$height, bmi = input$bmi
+    )
     data.frame(
       "Metric" = c(
         "VO2 ml/min", "VO2 ml/kg/min", "Heart rate", "Ventilation",
@@ -41,7 +76,7 @@ server <- function(input, output) {
       "Value" = c(
         vo2_ml_min(),
         vo2_ml_kg_min(),
-        heart_rate(input$diagnosis, input$height, input$bmi, as.numeric(input$sex)),
+        diag$heart_rate(diag, person),
         ventilation(),
         oxygen_pulse(),
         ve_vco2_slope(),
