e8bb5a2e0c5f055fe4f2023d9394966a369300eb
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-08-22 10:16:51 +0200

    Bumped version

--- src/ui.R
+++ src/ui.R
@@ -1,5 +1,5 @@
 ui <- fluidPage(
-  titlePanel("Kids with Congenital Heart Defects - v0.0.0.9006"),
+  titlePanel("Kids with Congenital Heart Defects - v0.0.0.9007"),
   sidebarPanel(
     # Step 1: selecting diagnostic group
     h2("Select group"),
