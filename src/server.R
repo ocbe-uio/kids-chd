8ebc3e7178f1e3ea812961d02b97bb4cd28c0d27
Waldir Leoncio <w.l.netto@medisin.uio.no>
2026-03-09 14:55:28 +0100

    ✨ Feat: remove plots from pdf report

--- src/server.R
+++ src/server.R
@@ -2,6 +2,7 @@ source("classes.R")
 source("methods.R")
 source("models.R")
 source("plotting.R")
+library(shinyjs)
 
 server <- function(input, output) {
   # Mapping of group IDs to display names (used in multiple plots)
@@ -391,8 +392,13 @@ server <- function(input, output) {
     )
   })
 
+  observe({
+    shinyjs::enable("download_pdf_btn")
+  })
+
   output$download_pdf <- downloadHandler(
     filename = function() {
+      shinyjs::disable("download_pdf_btn")
       paste0("kids-chd-report-", Sys.Date(), ".pdf")
     },
     content = function(file) {
@@ -420,6 +426,7 @@ server <- function(input, output) {
         # that source("classes.R") etc. inside the Rmd resolve correctly.
         knit_root_dir = getwd()
       )
+      shinyjs::enable("download_pdf_btn")
     }
   )
 
